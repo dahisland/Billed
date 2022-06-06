f33298304d2c699c97b4c3ee5bfc75464274ab3f
Myriam Mornet <dahisland@gmail.com>
2022-06-06 16:16:59 +0200

    RESOLVED - [bug report BillsUI descending order] - Test for display bills in function of date by descending order SUCCEED

--- Billed-app-FR-Front/src/views/BillsUI.js
+++ Billed-app-FR-Front/src/views/BillsUI.js
@@ -1,11 +1,11 @@
-import VerticalLayout from './VerticalLayout.js'
-import ErrorPage from "./ErrorPage.js"
-import LoadingPage from "./LoadingPage.js"
+import VerticalLayout from "./VerticalLayout.js";
+import ErrorPage from "./ErrorPage.js";
+import LoadingPage from "./LoadingPage.js";
 
-import Actions from './Actions.js'
+import Actions from "./Actions.js";
 
 const row = (bill) => {
-  return (`
+  return `
     <tr>
       <td>${bill.type}</td>
       <td>${bill.name}</td>
@@ -16,16 +16,58 @@ const row = (bill) => {
         ${Actions(bill.fileUrl)}
       </td>
     </tr>
-    `)
-  }
+    `;
+};
 
 const rows = (data) => {
-  return (data && data.length) ? data.map(bill => row(bill)).join("") : ""
-}
+  // Code to modify array elements order containing data by descending order in function of their date
+
+  const monthsDateFR = [
+    "Jan.",
+    "Fev.",
+    "Mar.",
+    "Avr.",
+    "Mai",
+    "Juin",
+    "Juil.",
+    "Aou.",
+    "Sep.",
+    "Oct.",
+    "Nov.",
+    "Dec.",
+  ];
+
+  // For each data obj, data.dataCompare store date in format YYYY-MM-DD
+  data.forEach((element) => {
+    let formatedDate = element.date;
+    let arrFormatedDate = formatedDate.split(" ");
+    arrFormatedDate.reverse();
+    // Year format
+    arrFormatedDate[0] = "20" + arrFormatedDate[0];
+    // Month format
+    arrFormatedDate[1] = monthsDateFR.indexOf(arrFormatedDate[1]) + 1;
+    if (arrFormatedDate[1] < 10) {
+      arrFormatedDate[1] = "0" + arrFormatedDate[1];
+    }
+    // Day format
+    if (arrFormatedDate[2] < 10) {
+      arrFormatedDate[2] = "0" + arrFormatedDate[2];
+    }
+    // Storage date format
+    element.dateCompare =
+      arrFormatedDate[0] + "-" + arrFormatedDate[1] + "-" + arrFormatedDate[2];
+  });
+
+  // data sorted by descending order in function of data.dateCompare
+  data = data.sort((a, b) => (a.dateCompare < b.dateCompare ? 1 : -1));
+
+  // END bug correction code
+
+  return data && data.length ? data.map((bill) => row(bill)).join("") : "";
+};
 
 export default ({ data: bills, loading, error }) => {
-  
-  const modal = () => (`
+  const modal = () => `
     <div class="modal fade" id="modaleFile" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
       <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
         <div class="modal-content">
@@ -40,15 +82,15 @@ export default ({ data: bills, loading, error }) => {
         </div>
       </div>
     </div>
-  `)
+  `;
 
   if (loading) {
-    return LoadingPage()
+    return LoadingPage();
   } else if (error) {
-    return ErrorPage(error)
+    return ErrorPage(error);
   }
-  
-  return (`
+
+  return `
     <div class='layout'>
       ${VerticalLayout(120)}
       <div class='content'>
@@ -75,6 +117,5 @@ export default ({ data: bills, loading, error }) => {
         </div>
       </div>
       ${modal()}
-    </div>`
-  )
-}
+    </div>`;
+};
